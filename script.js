48475a1d2a758a567e071c0de3784245af9f93e3
Ilija <ilija@htl3r.at>
2016-11-17 10:23:22 +0000

    scripts eingebunden

--- script.js
+++ script.js
@@ -2,11 +2,28 @@
  * Created by marco on 17.11.16.
  * ilija
  */
+function draw(){
 
-var stage = new createjs.Stage("mystage");
+    // zunächst muss ich eine Stage erzeugen, auf der gezeichnet werden kann
+    stage = new createjs.Stage(document.getElementById("myCanvas"));
 
-var circle = new createjs.Shape();
-circle.graphics.beginFill("red").drawCircle(0, 0, 50);
-circle.x = 100;
-circle.y = 100;
-stage.addChild(circle);
+// jetzt erzeuge ich ein Graphics-Objekt mit einer Linienfarbe
+    var g = new createjs.Graphics();
+    g.beginStroke('#ffffff');
+
+//jetzt geht es ans Zeichnen
+    g.moveTo(0,0);
+    g.lineTo(600,400);
+
+// wir sind mit dem Zeichnen fertig und wollen nun unser
+// Graphics-Object in eine Shape überführen, damit diese auf
+// der Bühne angezeigt werden kann.
+    var s = new createjs.Shape(g);
+
+// die Shape ist fertig, aber es muss noch auf die Bühne gebracht werden
+// und dann - man denke daran, dass im Canvas bei Änderungen immer die ganze
+// Bühne neu gezeichnet werden muss - wird mit der Methode update() alles neu gezeichnet
+    stage.addChild(s);
+    stage.update();
+}
+windows.addEventListener("load",draw,false);
