73dd1c13e8225aed2174c1d1ee81d1c40e9c2f5c
marco auenhammer <marco.auenhammer@gmail.com>
2016-11-17 10:48:30 +0100

    init2

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by marco on 17.11.16.
+ */
+
+var stage = new createjs.Stage("mystage");
+
+var circle = new createjs.Shape();
+circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+circle.x = 100;
+circle.y = 100;
+stage.addChild(circle);
