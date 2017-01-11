6331e503552b14d99548e6a6cd06baa717d17a1a
marco auenhammer <marco.auenhammer@gmail.com>
2017-01-11 12:12:09 +0100

    cleanup

--- js/level2.js
+++ js/level2.js
@@ -5,8 +5,6 @@
 var lines = [];
 
 var stage,
-    stageX,
-    stageY,
     stageHeight,
     stageWidth,
     playerObject,
@@ -14,7 +12,8 @@ var stage,
     playerObjectY,
     startX,
     startY,
-    line;
+    line,
+    obstacleContainer;
 
 
 function init() {
@@ -22,30 +21,69 @@ function init() {
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
 
+    stageHeight = stage.canvas.height;
+    stageWidth = stage.canvas.width;
+
+    startX = 0;
+    startY = 0;
+
+    playerObject = new createjs.canvas.
 
     console.log("loaded");
-
-
-    line = new createjs.Shape();
-    line.graphics.setStrokeStyle(5).beginStroke("#ffffff");
-    line.graphics.moveTo(0, 0);
-    stage.addChild(line);
+    console.log("starting game...");
+    startGame();
 }
 
 /**
  * Adds a new line to the planned routes and stores it as array in lines
  */
 function addLine(){
-    lines.push([parseInt(document.getElementById("x").value), parseInt(document.getElementById("y").value)]);
+    var x = parseInt(document.getElementById("x").value);
+    var y = parseInt(document.getElementById("y").value);
 
-    drawLine();
-    console.log(lines);
+    if(x > stageWidth || x < 0 || y > stageHeight || y < 0){
+        console.log("out of space, try again");
+    }else {
+        lines.push([x, y]);
+        drawLine();
+        console.log(lines);
+    }
 }
 
+/**
+ * Draws the line from the given input
+ */
 function drawLine() {
     line.graphics.lineTo(lines[lines.length - 1][0], lines[lines.length - 1][1]);
     stage.update();
     console.log("adding further lines, draw");
+}
+
+/**
+ * Creates the asteroids
+ */
+function createObstacles(){
+    stage.addChild(obstacleContainer);
+    for(var i = 0; i < 50; i++){
+        var g = new createjs.Graphics().setStrokeStyle(1).beginStroke("#ffffff").beginFill("white").drawCircle(stageWidth-Math.floor(Math.random()*1000), stageHeight-Math.floor(Math.random()*1000), 30);
+        var obstacle = new createjs.Shape(g);
+        stage.addChild(obstacle);
+    }
+}
+
+/**
+ * Starts the game
+ */
+function startGame() {
+    //lines begin to draw at left upper corner
+    line = new createjs.Shape();
+    line.graphics.setStrokeStyle(5).beginStroke("#ffffff");
+    line.graphics.moveTo(0, 0);
+    stage.addChild(line);
+
+    createObstacles();
+
+    stage.update();
 }
 
 window.addEventListener("loaded", init, false);
