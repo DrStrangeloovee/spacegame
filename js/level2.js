9e13b2ef1c169069b8d3e507294fad39c8f1f26c
marco <marco.auenhammer@gmail.com>
2017-01-18 18:36:51 +0100

    moving obstacles moving playerobject with mouse works movement line still needs to be drawn

--- js/level2.js
+++ js/level2.js
@@ -8,14 +8,23 @@ var stage,
     stageHeight,
     stageWidth,
     playerObject,
-    playerObjectX,
-    playerObjectY,
     startX,
     startY,
     line,
-    obstacleContainer,
     targetX,
     targetY;
+
+
+//line which is drawn when ship is moved
+var movementLine;
+
+var obstacles = [];
+
+//amount of obstacles to be created
+var amountObstacles = 20;
+
+//playerState
+var alive = true;
 
 var currentTargetNumber = 0;
 
@@ -24,8 +33,10 @@ function init() {
 
     stage = new createjs.Stage("stage");
     createjs.Ticker.setFPS(60);
-    createjs.Ticker.setInterval(30);
+    createjs.Ticker.setInterval(120);
     createjs.Ticker.addEventListener("tick", stage);
+    createjs.Ticker.addEventListener("tick", tick);
+    stage.enableMouseOver(60);
 
     stageHeight = stage.canvas.height;
     stageWidth = stage.canvas.width;
@@ -33,20 +44,47 @@ function init() {
     startX = 0;
     startY = 0;
 
-    var g = new createjs.Graphics().setStrokeStyle(1).beginStroke("red").beginFill("red").drawCircle(0, 0, 30);
-    playerObject = new createjs.Shape(g);
-    stage.addChild(playerObject);
+    movementLine = new createjs.Shape();
+    movementLine.graphics.setStrokeStyle(5).beginStroke("#ffffff");
+    movementLine.graphics.moveTo(0, 0);
 
     console.log("loaded");
     console.log("starting game...");
     startGame();
 }
 
+function createPlayerObject() {
+    playerObject = new createjs.Shape();
+    playerObject.graphics.setStrokeStyle(1).beginStroke("red").beginFill("red").drawCircle(0, 0, 15);
+    playerObject.x = 0;
+    playerObject.y = 0;
+    stage.addChild(playerObject);
+}
+
 /**
  * handles the tick
  */
 function tick(event) {
-    console.log("tick tock");
+    playerObject.on("pressmove", function(event) {
+        event.target.x = event.stageX;
+        event.target.y = event.stageY;
+    });
+    playerObject.on("pressup", function(event) { console.log("up"); });
+    //obstacles move here
+    if(alive){
+        //moveObstacles();
+    }else{
+        endGame();
+    }
+    stage.update();
+}
+/**
+ * Ends the somehow
+ * @returns {boolean}
+ */
+function endGame() {
+    console.log("dead");
+    return false;
 }
 
 /**
@@ -84,18 +122,28 @@ function drawLine() {
  * Creates the asteroids
  */
 function createObstacles(){
-    stage.addChild(obstacleContainer);
-    for(var i = 0; i < 50; i++){
-        var g = new createjs.Graphics().setStrokeStyle(1).beginStroke("#ffffff").beginFill("white").drawCircle(stageWidth-Math.floor(Math.random()*1000), stageHeight-Math.floor(Math.random()*1000), 30);
-        var obstacle = new createjs.Shape(g);
+    for(var i = 0; i < amountObstacles; i++){
+        var obstacle = new createjs.Shape();
+        obstacle.graphics.setStrokeStyle(1).beginStroke("#ffffff").beginFill("white").drawCircle(2,2,25);
+        obstacle.x = stageWidth-Math.floor(Math.random()*1000);
+        obstacle.y = stageHeight-Math.floor(Math.random()*1000);
+        //g.setBounds();
+        obstacle.name = "obst_" + i;
+        obstacles.push(obstacle);
         stage.addChild(obstacle);
+        console.log(stage.getChildAt(i));
     }
 }
 /**
  * after game hast started, the obstacles start to move from right to left
  */
 function moveObstacles() {
-
+    //console.log("in move");
+    for(var i in obstacles){
+        //console.log(obstacles[i].name + " moves from x: " + obstacles[i].x + " y: " + obstacles[i].y);
+        createjs.Tween.get(obstacles[i], { loop: false}).to({ x: 0, y: obstacles[i].y}, 20000).addEventListener("complete");
+        stage.update();
+    }
 }
 
 /**
@@ -104,13 +152,14 @@ function moveObstacles() {
 function startGame() {
     //lines begin to draw at left upper corner
 
-
-
+    //old stuff
+    /*
     var g = new createjs.Graphics().setStrokeStyle(5).beginStroke("#ffffff");
     line = new createjs.Shape(g);
     line.graphics.moveTo(0, 0);
     stage.addChild(line);
-
+*/
+    createPlayerObject();
     createObstacles();
 
     stage.update();
@@ -133,7 +182,11 @@ function fly() {
  * After the tween animation is finished you can do here some stuff, for now it only updates the playerObject coordinates to the new ones
  */
 function handleDestination() {
-    console.log("tween completed");
+
+
+
+    //old stuff
+/*    console.log("tween completed");
     playerObjectX = playerObject.x;
     playerObjectY = playerObject.y;
     targetX = lines[currentTargetNumber][0];
@@ -142,7 +195,7 @@ function handleDestination() {
     console.log(playerObjectX);
     console.log(playerObjectY);
     stage.update();
-    currentTargetNumber += 1;
+    currentTargetNumber += 1;*/
 }
 
 window.addEventListener("loaded", init, false);
